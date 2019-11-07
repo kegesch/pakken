f463df9b62295f1237aca3ae1e407929177e26d9
Jonas Geschke <mail@yonny.de>
2019-11-07 13:57:35 +0100

    added better error handling for pest's parsing errors

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,7 +2,7 @@ use ast::Namespace;
 use parser::parse;
 
 fn main() {
-    let code = "io.test {}";
+    let code = "io.test {";
     println!("Parsing! {:?}", code);
     println!("Result: {:?}", parse(code));
 }
